426cb2be703d545cb8f15d0aa7ab3fefcab5f506
mmoya26 <mmoya18@icloud.com>
2023-04-12 21:09:48 -0500

    Update cursor to pointer for filters

--- src/components/ScheduleDayFilter.js
+++ src/components/ScheduleDayFilter.js
@@ -2,14 +2,14 @@ import React from "react";
 
 export default function ScheduleDayFilter({ handleChange, day, id }) {
   return (
-    <div className="flex gap-2">
+    <div className="flex gap-2 cursor-pointer">
       <input
         id={id}
         type="checkbox"
         className="checkbox"
         onChange={(e) => handleChange(e, day)}
       />
-      <label htmlFor={id} className="text-slate-600 text-base">
+      <label htmlFor={id} className="text-slate-600 text-base cursor-pointer">
         {day}
       </label>
     </div>
