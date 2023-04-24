4ca7042e26d6a783073b152a3c217ecf42bb4acc
mmoya26 <mmoya18@icloud.com>
2023-04-24 16:08:39 -0500

    Create helper file to determine badge colors depending on text passed + Replace strings ON,OFF,OPS,OC,D with badges

--- src/components/ScheduleLineItem.js
+++ src/components/ScheduleLineItem.js
@@ -1,37 +1,39 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { generateScheduleBadge } from "@/helpers/generateScheduleBadge";
 
 export default function ScheduleLineItem(props) {
-  const { n, removeEmployee } = props;
-  const { id, name, daysWorking } = props.row;
-  // text-emerald-500
-  // red-500
-  return (
-    <div
-      className="flex border-solid border border-grey-200 rounded-[12px] items-center mb-3"
-      style={{
-        backgroundColor: `${n % 2 === 0 ? "#f8fafc" : "#f1f5f9"}`,
-      }}>
-      <div className="text-slate-600 basis-1/4 p-4 text-center">{name}</div>
-      <div className="flex gap-[60px] text-center">
-        {Object.keys(daysWorking).map((day, i) => {
-          return (
-            <div
-              className={`py-4 w-[93px] ${
-                daysWorking[day] !== "off" ? "text-emerald-500" : "text-red-500"
-              }`}
-              key={i}>
-              {daysWorking[day].toUpperCase()}
-            </div>
-          );
-        })}
-      </div>
-      <FontAwesomeIcon
-        icon={faTrash}
-        className="w-[46px] p-4 ml-auto text-slate-500 cursor-pointer"
-        onClick={() => removeEmployee(id)}
-      />
-    </div>
-  );
+	const { n, removeEmployee } = props;
+	const { id, name, daysWorking } = props.row;
+	// text-emerald-500
+	// red-500
+	return (
+		<div
+			className="flex border-solid border border-grey-200 rounded-[12px] items-center mb-3"
+			style={{
+				backgroundColor: `${n % 2 === 0 ? "#f8fafc" : "#f1f5f9"}`,
+			}}
+		>
+			<div className="text-slate-600 basis-1/4 p-4 text-center">{name}</div>
+			<div className="flex gap-[60px] text-center">
+				{Object.keys(daysWorking).map((day, i) => {
+					return (
+						<div className="py-4 w-[93px]" key={i}>
+							{/* {daysWorking[day].toUpperCase()} */}
+							{/* <span class="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
+								OFF
+							</span> */}
+							{generateScheduleBadge(daysWorking[day].toUpperCase())}
+						</div>
+					);
+				})}
+			</div>
+			<FontAwesomeIcon
+				icon={faTrash}
+				className="w-[46px] p-4 ml-auto text-slate-500 cursor-pointer"
+				onClick={() => removeEmployee(id)}
+			/>
+		</div>
+	);
 }
---
+++ src/helpers/generateScheduleBadge.js
@@ -0,0 +1,48 @@
+function generateScheduleBadge(text) {
+	console.log(text);
+	switch (text) {
+		case "ON":
+			return (
+				<span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+					{text}
+				</span>
+			);
+		case "OFF":
+			return (
+				<span className="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
+					{text}
+				</span>
+			);
+
+		case "OPS":
+			return (
+				<span className="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">
+					{text}
+				</span>
+			);
+
+		case "D": {
+			return (
+				<span className="inline-flex items-center rounded-md bg-purple-50 px-2 py-1 text-xs font-medium text-purple-700 ring-1 ring-inset ring-purple-700/10">
+					{text}
+				</span>
+			);
+		}
+
+		case "OC":
+			return (
+				<span className="inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20">
+					{text}
+				</span>
+			);
+
+		default:
+			return (
+				<span className="inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20">
+					Badge
+				</span>
+			);
+	}
+}
+
+export { generateScheduleBadge };
