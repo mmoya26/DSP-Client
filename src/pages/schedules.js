f863d5af69e9bd02e0c47f880e4b0c13201c93d4
mmoya26 <mmoya18@icloud.com>
2023-04-25 20:42:33 -0500

    Add edit button icon + make h1 date larger

--- src/pages/schedules.js
+++ src/pages/schedules.js
@@ -7,13 +7,12 @@ import IconButton from "@mui/material/IconButton";
 import CloseIcon from "@mui/icons-material/Close";
 import uuid from "react-uuid";
 import {
-	faChevronLeft,
-	faChevronRight,
-	faPlus,
+  faChevronLeft,
+  faChevronRight,
 } from "@fortawesome/free-solid-svg-icons";
 import {
-	EditScheduleContext,
-	EditScheduleUpdateContext,
+  EditScheduleContext,
+  EditScheduleUpdateContext,
 } from "@/contexts/EditScheduleContext";
 import AddUserDialog from "@/components/AddUserDialog";
 import ScheduleSearchBar from "@/components/ScheduleSearchBar";
@@ -21,131 +20,129 @@ import ScheduleFilterContainer from "@/components/ScheduleFilterContainer";
 import ScheduleAddEmployeeButton from "@/components/ScheduleAddEmployeeButton";
 
 const daysOfTheWeek = [
-	"Sunday",
-	"Monday",
-	"Tuesday",
-	"Wednesday",
-	"Thursday",
-	"Friday",
-	"Saturday",
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
 ];
 
 export default function Schedules() {
-	const { rows, activeFilters, snackBarState } =
-		useContext(EditScheduleContext);
-	const { handleSnackBarClose, handleSnackBar, dispatch } = useContext(
-		EditScheduleUpdateContext
-	);
+  const { rows, activeFilters, snackBarState } =
+    useContext(EditScheduleContext);
+  const { handleSnackBarClose, handleSnackBar, dispatch } = useContext(
+    EditScheduleUpdateContext
+  );
 
-	const action = (
-		<React.Fragment>
-			{/* <Button color="secondary" size="small" onClick={handleClose}>
+  const action = (
+    <React.Fragment>
+      {/* <Button color="secondary" size="small" onClick={handleClose}>
         UNDO
       </Button> */}
-			<IconButton
-				size="small"
-				aria-label="close"
-				color="inherit"
-				onClick={handleSnackBarClose}
-			>
-				<CloseIcon fontSize="small" />
-			</IconButton>
-		</React.Fragment>
-	);
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleSnackBarClose}>
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </React.Fragment>
+  );
 
-	return (
-		<div className="mx-auto mt-5 max-w-screen-2xl">
-			<h1 className="text-slate-600 font-bold text-2xl mb-12">
-				<p className="text-slate-500 font-normal text-base">
-					Schedule for week of:
-				</p>
-				May 5/1 - 5/7
-			</h1>
+  return (
+    <div className="mx-auto mt-5 max-w-screen-2xl">
+      <h1 className="text-slate-600 font-bold text-3xl mb-12">
+        <p className="text-slate-500 font-normal text-base">
+          Schedule for week of:
+        </p>
+        May 5/1 - 5/7
+      </h1>
 
-			{/* Filters */}
-			<ScheduleFilterContainer />
+      {/* Filters */}
+      <ScheduleFilterContainer />
 
-			{/* Search Bar */}
-			<ScheduleSearchBar />
+      {/* Search Bar */}
+      <ScheduleSearchBar />
 
-			{/* Days of the week */}
-			<div className="flex mt-6 items-center">
-				<div className="text-slate-600 basis-1/4 px-4 text-center">
-					Employee Name
-				</div>
-				<div className="flex gap-[60px]">
-					{daysOfTheWeek.map((d, i) => {
-						const isActive = activeFilters.includes(d.toLowerCase());
+      {/* Days of the week */}
+      <div className="flex mt-6 items-center">
+        <div className="text-slate-600 basis-1/4 px-4 text-center">
+          Employee Name
+        </div>
+        <div className="flex gap-[60px]">
+          {daysOfTheWeek.map((d, i) => {
+            const isActive = activeFilters.includes(d.toLowerCase());
 
-						return (
-							<div
-								className={`text-slate-600 text-center w-[93px] ${
-									isActive ? "font-bold" : ""
-								}`}
-								key={uuid()}
-							>
-								{d} <br />
-								(5/{i + 1})
-							</div>
-						);
-					})}
-				</div>
-				<div className="w-[46px]"></div>
-			</div>
+            return (
+              <div
+                className={`text-slate-600 text-center w-[93px] ${
+                  isActive ? "font-bold" : ""
+                }`}
+                key={uuid()}>
+                {d} <br />
+                (5/{i + 1})
+              </div>
+            );
+          })}
+        </div>
+        <div className="w-[46px]"></div>
+      </div>
 
-			{/* Table Structure */}
-			<div className="mt-5">
-				{/* Line Item */}
-				{rows.map((r, n) => {
-					return (
-						<ScheduleLineItem
-							n={n}
-							row={r}
-							removeEmployee={() => {
-								dispatch({ type: "REMOVE", id: r.id });
-								handleSnackBar("Employee sucessfuly removed");
-							}}
-							key={r.id}
-						/>
-					);
-				})}
-			</div>
+      {/* Table Structure */}
+      <div className="mt-5">
+        {/* Line Item */}
+        {rows.map((r, n) => {
+          return (
+            <ScheduleLineItem
+              n={n}
+              row={r}
+              removeEmployee={() => {
+                dispatch({ type: "REMOVE", id: r.id });
+                handleSnackBar("Employee sucessfuly removed");
+              }}
+              key={r.id}
+            />
+          );
+        })}
+      </div>
 
-			{/* Pagination */}
-			<div className="mt-6 mb-10 flex items-center justify-between text-base">
-				<ScheduleAddEmployeeButton />
-				<div className="flex items-center gap-5">
-					<span className="text-slate-500 text-base">Page 1 of 10</span>
-					<div className="flex gap-3">
-						{/* Reduce opacity of left or right arrow depending if there is any available pages on either
+      {/* Pagination */}
+      <div className="mt-6 mb-10 flex items-center justify-between text-base">
+        <ScheduleAddEmployeeButton />
+        <div className="flex items-center gap-5">
+          <span className="text-slate-500 text-base">Page 1 of 10</span>
+          <div className="flex gap-3">
+            {/* Reduce opacity of left or right arrow depending if there is any available pages on either
             direction */}
-						<button className="w-8 h-8 rounded-[3px] border border-gray-200 shadow-sm">
-							<FontAwesomeIcon
-								icon={faChevronLeft}
-								className="text-slate-500 cursor-pointer text-base"
-							/>
-						</button>
-						<button className="w-8 h-8 rounded-[3px] border border-gray-200 shadow-sm">
-							<FontAwesomeIcon
-								icon={faChevronRight}
-								className="text-slate-500 cursor-pointer text-base"
-							/>
-						</button>
-					</div>
-				</div>
-			</div>
+            <button className="w-8 h-8 rounded-[3px] border border-gray-200 shadow-sm">
+              <FontAwesomeIcon
+                icon={faChevronLeft}
+                className="text-slate-500 cursor-pointer text-base"
+              />
+            </button>
+            <button className="w-8 h-8 rounded-[3px] border border-gray-200 shadow-sm">
+              <FontAwesomeIcon
+                icon={faChevronRight}
+                className="text-slate-500 cursor-pointer text-base"
+              />
+            </button>
+          </div>
+        </div>
+      </div>
 
-			<AddUserDialog />
-			<Snackbar
-				open={snackBarState.open}
-				autoHideDuration={6000}
-				onClose={handleSnackBarClose}
-				message={snackBarState.message}
-				action={action}
-				ContentProps={{
-					className: "bg-slate-600",
-				}}
-			/>
-		</div>
-	);
+      <AddUserDialog />
+      <Snackbar
+        open={snackBarState.open}
+        autoHideDuration={6000}
+        onClose={handleSnackBarClose}
+        message={snackBarState.message}
+        action={action}
+        ContentProps={{
+          className: "bg-slate-600",
+        }}
+      />
+    </div>
+  );
 }
